505acc069272d90e13ee84f071ac24e57a1cdb34
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-05-21 17:32:29 +0700

    Update AdjacentPostCard.jsx

--- components/AdjacentPostCard.jsx
+++ components/AdjacentPostCard.jsx
@@ -21,7 +21,7 @@ const AdjacentPostCard = ({ post, position }) => (
       <span className="z-10 cursor-pointer absolute w-full h-full" />
     </Link>
     {position === 'LEFT' && (
-      <div className="absolute arrow-btn bottom-5 text-center py-3 cursor-pointer bg-pink-600 left-4 rounded-full">
+      <div className="absolute arrow-btn bottom-5 text-center py-3 cursor-pointer bg-blue-600 left-4 rounded-full">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="block ml-3 h-6 w-6 text-white w-full"
@@ -40,7 +40,7 @@ const AdjacentPostCard = ({ post, position }) => (
       </div>
     )}
     {position === 'RIGHT' && (
-      <div className="absolute arrow-btn bottom-5 text-center py-3 cursor-pointer bg-pink-600 right-4 rounded-full">
+      <div className="absolute arrow-btn bottom-5 text-center py-3 cursor-pointer bg-blue-600 right-4 rounded-full">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="block ml-3 h-6 w-6 text-white w-full"
